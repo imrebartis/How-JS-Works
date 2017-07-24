bebc4db3e9473ef31b759859be33b8afe640d71f
imrebartis <imbartis@gmail.com>
2017-07-24 17:37:56 +0300

    game is played with 2 dices, if one of them is a 1, player looses current score

--- Pig-Game/challenges.js
+++ Pig-Game/challenges.js
@@ -8,6 +8,7 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 - A player looses his entire score when he rolls two 6 in a row. After that, it's the other player's turn
 - There's an input field where players can set the winning score
+- Add another dice to the game. Player looses current score when one of them is a 1
 
 */
 
@@ -31,32 +32,45 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
 
 		console.log('hi')
 
-	var dice = Math.floor(Math.random()*6)+1;
+	var dice1 = Math.floor(Math.random()*6)+1;
+	var dice2 = Math.floor(Math.random()*6)+1;
 
 	//Display result
-	var diceDOM = document.querySelector('.dice');
-	diceDOM.style.display = 'block';
-	diceDOM.src = 'dice-' + dice + '.png';
+	document.getElementById('dice-1').style.display = 'block';
+	document.getElementById('dice-2').style.display = 'block';
+	document.getElementById('dice-1').src = 'dice-' + dice1 + '.png';
+	document.getElementById('dice-2').src = 'dice-' + dice2 + '.png';
 
 	//Update score if rolled number was not 1
 
-	if(dice === 6 && lastDice === 6) {
-		//Player looses score
-		scores[activePlayer] = 0;
-		document.querySelector('#score-' + activePlayer).textContent = '0';
-		nextPlayer();
-	} else if (dice !== 1) {
+	if (dice1 !== 1 && dice2 !== 1) {
 		//add score
-		roundScore += dice;
+		roundScore += dice1 + dice2;
 		document.querySelector('#current-' + activePlayer).textContent = roundScore;
 	}
 	else {
 		nextPlayer();
 	}
-
-	lastDice = dice;
   }
 });
+
+// 	if(dice === 6 && lastDice === 6) {
+// 		//Player looses score
+// 		scores[activePlayer] = 0;
+// 		document.querySelector('#score-' + activePlayer).textContent = '0';
+// 		nextPlayer();
+// 	} else if (dice !== 1) {
+// 		//add score
+// 		roundScore += dice;
+// 		document.querySelector('#current-' + activePlayer).textContent = roundScore;
+// 	}
+// 	else {
+// 		nextPlayer();
+// 	}
+
+// 	lastDice = dice;
+//   }
+// });
 
 document.querySelector('.btn-hold').addEventListener('click', function(){
 
@@ -83,7 +97,8 @@ document.querySelector('.btn-hold').addEventListener('click', function(){
 
 		if (scores[activePlayer] >= winningScore) {
 			document.querySelector('#name-' + activePlayer).textContent = "Winner";
-			document.querySelector('.dice').style.display = 'none';
+			document.getElementById('dice-1').style.display = 'none';
+			document.getElementById('dice-2').style.display = 'none';
 			document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
 			document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
 			gamePlaying = false; //thus we stop the game
@@ -109,7 +124,8 @@ function nextPlayer(){
 		document.querySelector('.player-1-panel').classList.toggle('active');
 
 		//hiding the dice before the other player throws it for the first time in his round
-		document.querySelector('.dice').style.display = 'none';
+		document.getElementById('dice-1').style.display = 'none';
+		document.getElementById('dice-2').style.display = 'none';
 }
 
 document.querySelector('.btn-new').addEventListener('click', init);
@@ -122,7 +138,8 @@ function init() {
 
 	document.getElementById('score-0').textContent = '0';
 	document.getElementById('score-1').textContent = '0';
-	document.querySelector('.dice').style.display = 'none';
+	document.getElementById('dice-1').style.display = 'none';
+	document.getElementById('dice-2').style.display = 'none';
 
 	document.getElementById('name-0').textContent = "Player1";
 	document.getElementById('name-1').textContent = "Player2";
