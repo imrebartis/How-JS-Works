d7810b7749c71ac94e4530ca1c67e1e3d3ff008f
imrebartis <imbartis@gmail.com>
2017-08-11 17:47:03 +0300

    adding id of clicked table cell to a ul exercise updated

--- adding id of clicked table cell to a ul/app.js
+++ adding id of clicked table cell to a ul/app.js
@@ -1,13 +1,11 @@
 window.onload = function(){
     document.getElementById('tableID').onclick = function(e){
-        var e = e || window.event;
+        //var e = e || window.event;
         var target = e.target || e.srcElement;
-        var table =  document.getElementById('tableID');
-        var cells = table.getElementsByTagName("td"); 
         var ul = document.getElementById("list");
         var li = document.createElement("li");
         
-        if(target.tagName.toLowerCase() ==  "td") {
+        if(target.tagName ===  "TD") {
             //alert(target.innerHTML);
             li.innerHTML=target.id;
             ul.appendChild(li);
