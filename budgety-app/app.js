10903ccf1c2663d30f367718494ed5ce5a6a6bc7
imrebartis <imbartis@gmail.com>
2017-11-05 03:26:31 +0200

    delete item from UI

--- budgety-app/app.js
+++ budgety-app/app.js
@@ -182,6 +182,14 @@ var UIController = (function() {
         },
         
         
+        deleteListItem: function(selectorID) {
+            
+            var el = document.getElementById(selectorID);
+            el.parentNode.removeChild(el);
+            
+        },
+        
+        
         clearFields: function() {
             var fields, fieldsArr;
             
@@ -293,11 +301,10 @@ var controller = (function(budgetCtrl, UICtrl) {
             budgetCtrl.deleteItem(type, ID);
             
             // 2. Delete the item from the UI
-            
-
+            UICtrl.deleteListItem(itemID);
             
             // 3. Update and show the new budget
-         
+         	updateBudget();
             
             // 4. Calculate and update percentages
           
